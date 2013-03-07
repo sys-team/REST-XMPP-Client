d91c93852c7e546ea320422b6282c3b81fe5018b
kovtash <kovtash@kovtash-mbp.oz>
2013-03-08 00:12:53 +0400

    Secure session and contacts ids

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 __author__ = 'kovtash'
 
 import os
-import json
 import logging
 from bottle import Bottle, template, request, abort
 from xmpp_session_pool import  XMPPAuthError, XMPPConnectionError, XMPPSendError, XMPPRosterError, XMPPSendError
@@ -23,22 +22,22 @@ def start_session(xmpp_pool):
     
     if jid is None or password is None or server is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
     
     try:
         session_id = xmpp_pool.start_session(jid,password,server)
         response['session']['session_id'] = session_id
     except XMPPAuthError:
         response['error'] = {'code':'XMPPAuthError','text':template('Service for {{jid}} can\'t authenticate on xmpp server',jid=jid)}
-        abort(502, json.dumps(response))
+        abort(502, response)
     except XMPPConnectionError as error:
         response['error'] = {'code':'XMPPConnectionError','text':template('Service can\'t connect to xmpp server {{server}}',server=error.server)}
-        abort(502, json.dumps(response))
+        abort(502, response)
     except XMPPSendError:
         response['error'] = {'code':'XMPPConnectionError','text':'Message was not sent'}
-        abort(502, json.dumps(response))
+        abort(502, response)
     
-    return json.dumps(response)
+    return response
     
 @app.route('/sessions/<session_id>')
 def session(xmpp_pool,session_id=None):
@@ -46,15 +45,15 @@ def session(xmpp_pool,session_id=None):
     
     if session_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
     
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
         
-    return json.dumps(response)
+    return response
 
 @app.route('/sessions/<session_id>/remove')
 def session(xmpp_pool,session_id=None):
@@ -62,15 +61,15 @@ def session(xmpp_pool,session_id=None):
 
     if session_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
     try:
         xmpp_pool.close_session(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
-    return json.dumps(response)
+    return response
     
 @app.route('/sessions/<session_id>/contacts')
 def session_contacts(xmpp_pool,session_id=None):
@@ -80,149 +79,161 @@ def session_contacts(xmpp_pool,session_id=None):
     
     if session_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
     if jid is None:
         response = {'contacts':{}}
         response['contacts'] = session.contacts()
     else:
         session.add_contact(jid)
-        response['contact'] = session.contact(jid)
+        response['contact'] = session.contactByJID(jid)
         
-    return json.dumps(response)
+    return response
     
-@app.route('/sessions/<session_id>/contacts/<jid>')
-def session_contact(xmpp_pool,session_id=None,jid=None):
+@app.route('/sessions/<session_id>/contacts/<contact_id>')
+def session_contact(xmpp_pool,session_id=None,contact_id=None):
     response = {}
     
-    if session_id is None or jid is None:
+    if session_id is None or contact_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
     
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
         
     try:
-        response['contact'] = session.contact(jid)
+        response['contact'] = session.contact(contact_id)
     except KeyError:
-        response['error'] = {'code':'XMPPSessionError','text':template('There is no contact with id {{jid}}',jid=jid)}
-        abort(404, json.dumps(response))
+        response['error'] = {'code':'XMPPSessionError','text':template('There is no contact with id {{contact_id}}',contact_id=contact_id)}
+        abort(404, response)
         
-    return json.dumps(response)
+    return response
 
-@app.route('/sessions/<session_id>/contacts/<jid>/authorize')
-def session_contact_authorize(xmpp_pool,session_id=None,jid=None):
+@app.route('/sessions/<session_id>/contacts/<contact_id>/authorize')
+def session_contact_authorize(xmpp_pool,session_id=None,contact_id=None):
     response = {}
 
-    if session_id is None or jid is None:
+    if session_id is None or contact_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
 
-    if jid in session.contacts():
-        session.authorize_contact(jid)
-        response['contact'] = session.contact(jid)
+    if contact_id in session.contacts():
+        session.authorize_contact(contact_id)
+        response['contact'] = session.contact(contact_id)
     else:
-        response['error'] = {'code':'XMPPSessionError','text':template('There is no contact with id {{jid}}',jid=jid)}
-        abort(404, json.dumps(response))
+        response['error'] = {'code':'XMPPSessionError','text':template('There is no contact with id {{contact_id}}',contact_id=contact_id)}
+        abort(404, response)
 
-    return json.dumps(response)
+    return response
 
-@app.route('/sessions/<session_id>/contacts/<jid>/remove')
-def session_contact_remove(xmpp_pool,session_id=None,jid=None):
+@app.route('/sessions/<session_id>/contacts/<contact_id>/remove')
+def session_contact_remove(xmpp_pool,session_id=None,contact_id=None):
     response = {}
 
-    if session_id is None or jid is None:
+    if session_id is None or contact_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
-    session.remove_contact(jid)
+    try:
+        session.remove_contact(contact_id)
+    except TypeError:
+        response['error'] = {'code':'XMPPContactError','text':template('There is no contact with id {{contact_id}}',contact_id=contact_id)}
+        abort(404, response)
 
-    return json.dumps(response)
+    return response
 
-@app.route('/sessions/<session_id>/contacts/<jid>/messages')
-def contact_messages(xmpp_pool,session_id=None,jid=None):
+@app.route('/sessions/<session_id>/contacts/<contact_id>/messages')
+def contact_messages(xmpp_pool,session_id=None,contact_id=None):
     message = request.query.get('message',None)
     response = {}
     
-    if session_id is None or jid is None:
+    if session_id is None or contact_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))    
+        abort(400, response)    
     
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
         
     if message is None:
-        response['messages'] = session.messages(jid)
-        return json.dumps(response)
-            
-    try:
-        session.send(jid,message)
-    except XMPPSendError:
-        response['error'] = {'code':'XMPPSendError','text':template('Message sending failed')}
-        abort(404, json.dumps(response))
+        try:
+            response['messages'] = session.messages(contact_id)
+        except TypeError:
+            response['error'] = {'code':'XMPPContactError','text':template('There is no contact with id {{contact_id}}',contact_id=contact_id)}
+            abort(404, response)
+    else:
+        try:
+            session.send(contact_id,message)
+        except XMPPSendError:
+            response['error'] = {'code':'XMPPSendError','text':template('Message sending failed')}
+            abort(404, response)
+
+        except TypeError:
+            response['error'] = {'code':'XMPPContactError','text':template('There is no contact with id {{contact_id}}',contact_id=contact_id)}
+            abort(404, response)
+
         
     response['message'] = message
         
-    return json.dumps(response)
+    return response
 
 @app.route('/sessions/<session_id>/messages')
 def session_messages(xmpp_pool,session_id=None):
     message = request.query.get('message',None)
-    jid = request.query.get('jid',None)
+    contact_id = request.query.get('contact_id',None)
     response = {}
 
     if session_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
     try:
         session = xmpp_pool.session_for_id(session_id)
     except KeyError:
         response['error'] = {'code':'XMPPSessionError','text':template('There is no session with id {{session_id}}',session_id=session_id)}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
-    if  message is None and jid is None:
+    if  message is None and contact_id is None:
         response['messages'] = session.all_messages()
-        return json.dumps(response)
+        return response
 
-    if message is None or jid is None:
+    if message is None or contact_id is None:
         response['error'] = {'code':'XMPPServiceParametersError','text':'Missing required parameters'}
-        abort(400, json.dumps(response))
+        abort(400, response)
 
-    response['message'] = {'jid':jid,'text':message}
+    response['message'] = {'jid':contact_id,'text':message}
 
     try:
-        response['message']['id'] = session.send(jid,message)
+        response['message']['id'] = session.send(contact_id,message)
     except XMPPSendError:
         response['error'] = {'code':'XMPPSendError','text':template('Message sending failed')}
-        abort(404, json.dumps(response))
+        abort(404, response)
 
-    return json.dumps(response)
+    return response
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
--- xmpp_session_pool.py
+++ xmpp_session_pool.py
@@ -3,11 +3,11 @@
 __author__ = 'kovtash'
 
 import xmpp
-import os
 import logging
 import threading
 import inspect
 import socket
+import uuid
 
 from bottle import PluginError
 
@@ -26,6 +26,113 @@ class XMPPSendError(Exception):
 class XMPPRosterError(Exception):
     pass
 
+class XMPPSecureRoster(xmpp.roster.Roster):
+    def __init__(self):
+        xmpp.roster.Roster.__init__(self)
+        self.uuid_namespace = uuid.uuid4()
+
+    def plugin(self,owner,request=1):
+        """ Register presence and subscription trackers in the owner's dispatcher.
+        Also request roster from server if the 'request' argument is set.
+        Used internally."""
+        self._owner.Dispatcher.RegisterHandler('iq',self.RosterIqHandler,'result',xmpp.protocol.NS_ROSTER)
+        self._owner.Dispatcher.RegisterHandler('iq',self.RosterIqHandler,'set',xmpp.protocol.NS_ROSTER)
+        self._owner.Dispatcher.RegisterHandler('presence',self.PresenceHandler)
+        if request: self.Request()
+
+    def itemId(self,jid):
+        return uuid.uuid3(self.uuid_namespace,jid.encode('utf8')).hex
+
+    def RosterIqHandler(self,dis,stanza):
+        """ Subscription tracker. Used internally for setting items state in
+            internal roster representation. """
+        for item in stanza.getTag('query').getTags('item'):
+            jid=item.getAttr('jid')
+            item_id = self.itemId(jid)
+
+            if item.getAttr('subscription')=='remove':
+                if self._data.has_key(item_id): del self._data[item_id]
+                raise xmpp.protocol.NodeProcessed             # a MUST
+            self.DEBUG('Setting roster item %s...'%item_id,'ok')
+            if not self._data.has_key(item_id): self._data[item_id]={}
+            self._data[item_id]['jid']=jid
+            self._data[item_id]['name']=item.getAttr('name')
+            self._data[item_id]['ask']=item.getAttr('ask')
+            self._data[item_id]['subscription']=item.getAttr('subscription')
+            self._data[item_id]['groups']=[]
+            if not self._data[item_id].has_key('resources'): self._data[item_id]['resources']={}
+            for group in item.getTags('group'): self._data[item_id]['groups'].append(group.getData())
+        self._data[self._owner.User+'@'+self._owner.Server]={'resources':{},'name':None,'ask':None,'subscription':None,'groups':None,}
+        self.set=1
+        raise xmpp.protocol.NodeProcessed   # a MUST. Otherwise you'll get back an <iq type='error'/>
+
+    def PresenceHandler(self,dis,pres):
+        """ Presence tracker. Used internally for setting items' resources state in
+            internal roster representation. """
+        jid=xmpp.protocol.JID(pres.getFrom())
+        item_id = self.itemId(jid.getStripped())
+
+        if not self._data.has_key(item_id): self._data[item_id]={'name':None,'ask':None,'subscription':'none','groups':['Not in roster'],'resources':{}}
+
+        item=self._data[item_id]
+        typ=pres.getType()
+
+        if not typ:
+            self.DEBUG('Setting roster item %s for resource %s...'%(jid.getStripped(),jid.getResource()),'ok')
+            item['resources'][jid.getResource()]=res={'show':None,'status':None,'priority':'0','timestamp':None}
+            if pres.getTag('show'): res['show']=pres.getShow()
+            if pres.getTag('status'): res['status']=pres.getStatus()
+            if pres.getTag('priority'): res['priority']=pres.getPriority()
+            if not pres.getTimestamp(): pres.setTimestamp()
+            res['timestamp']=pres.getTimestamp()
+        elif typ=='unavailable' and item['resources'].has_key(jid.getResource()): del item['resources'][jid.getResource()]
+        # Need to handle type='error' also
+
+    def _getItemData(self,jid,dataname):
+        """ Return specific jid's representation in internal format. Used internally. """
+        jid=jid[:(jid+'/').find('/')]
+        return self._data[self.itemId(jid)][dataname]
+    def _getResourceData(self,jid,dataname):
+        """ Return specific jid's resource representation in internal format. Used internally. """
+        if jid.find('/')+1:
+            jid,resource=jid.split('/',1)
+            item_id = self.itemId(jid)
+            if self._data[item_id]['resources'].has_key(resource): return self._data[item_id]['resources'][resource][dataname]
+        elif self._data[item_id]['resources'].keys():
+            lastpri=-129
+            for r in self._data[item_id]['resources'].keys():
+                if int(self._data[item_id]['resources'][r]['priority'])>lastpri: resource,lastpri=r,int(self._data[item_id]['resources'][r]['priority'])
+            return self._data[item_id]['resources'][resource][dataname]
+
+    def getRawItem(self,jid):
+        """ Returns roster item 'jid' representation in internal format. """
+        return self._data[self.itemId(jid[:(jid+'/').find('/')])]
+
+    def getResources(self,jid):
+        """ Returns list of connected resources of contact 'jid'."""
+        return self._data[self.itemId(jid[:(jid+'/').find('/')])]['resources'].keys()
+
+    def getItemByJID(self,jid):
+        print('by JID',self.itemId(jid))
+        return self.getItem(self.itemId(jid))
+
+class XMPPSecureClient(xmpp.Client):
+    def __init__(self,server,port=5222,debug=['always', 'nodebuilder']):
+        self.Namespace,self.DBG='jabber:client',xmpp.client.DBG_CLIENT
+        xmpp.Client.__init__(self,server,port,debug)
+
+    def getRoster(self):
+        """ Return the Roster instance, previously plugging it in and
+            requesting roster from server if needed. """
+        if not self.__dict__.has_key('Roster'): XMPPSecureRoster().PlugIn(self)
+        return self.Roster.getRoster()
+
+    def sendPresence(self,jid=None,typ=None,requestRoster=0):
+        """ Send some specific presence state.
+            Can also request roster from server if according agrument is set."""
+        if requestRoster: XMPPSecureRoster().PlugIn(self)
+        self.send(xmpp.dispatcher.Presence(to=jid, typ=typ))
+
 class XMPPSession():
     def __init__(self,jid,password,server=None):
         self.jid = xmpp.protocol.JID(jid)
@@ -33,15 +140,16 @@ class XMPPSession():
             server = self.jid.getDomain()
         self.password=password
         self.server = server
-        self.client = xmpp.Client(self.jid.getDomain(),debug = [])
+        #self.client = xmpp.Client(self.jid.getDomain(),debug = [])
+        self.client = XMPPSecureClient(self.jid.getDomain(),debug = [])
         self.messages_store = {}
-        self.client.RegisterDisconnectHandler(self.setup_connection)
+        self.client.RegisterDisconnectHandler(self.client.reconnectAndReauth)
         self.client.UnregisterDisconnectHandler(self.client.DisconnectHandler)
         self.setup_connection()
 
     def clean(self):
         logging.info('Session %s start cleaning', self.jid)
-        self.client.UnregisterDisconnectHandler(self.setup_connection)
+        self.client.UnregisterDisconnectHandler(self.client.reconnectAndReauth)
 
         if 'TCPsocket' in self.client.__dict__:
             sock = self.client.__dict__['TCPsocket']
@@ -94,12 +202,12 @@ class XMPPSession():
             
             self.register_handlers()
             self.client.sendInitPresence()
-            #self.process_thread.start()
             
-    def send(self,to_jid,message):
+    def send(self,contact_id,message):
         self.setup_connection()
         if self.client.isConnected():
-            id = self.client.send(xmpp.protocol.Message(to_jid,message))
+            jid = self.client.getRoster().getItem(contact_id)['jid']
+            id = self.client.send(xmpp.protocol.Message(jid,message))
             if not id:
                 raise XMPPSendError()
             return id
@@ -108,28 +216,29 @@ class XMPPSession():
             
     def contacts(self):
         if self.client.isConnected():
-            roster = self.client.getRoster()
-            items = roster.getItems()
-            agg_roster = {}
-            
-            for item in items:
-                agg_roster[item] = roster.getItem(item)
-                agg_roster[item]['status'] =  roster.getStatus(item)
-                agg_roster[item]['priority'] =  roster.getPriority(item)
-                
-            return agg_roster
+            return self.client.getRoster().getRawRoster()
         else:
             raise XMPPRosterError()
             
-    def contact(self,jid):
-        self.client.getRoster().getRoster()
+    def contact(self,contact_id):
         if self.client.isConnected():
-            return self.client.getRoster().getItem(jid)
+            contact = self.client.getRoster().getItem(contact_id)
+            if  contact is None:
+                raise KeyError
+            else:
+                return contact
+        else:
+            raise XMPPRosterError()
+
+    def contactByJID(self,jid):
+        if self.client.isConnected():
+            return self.client.getRoster().getItemByJID(jid)
         else:
             raise XMPPRosterError()
             
-    def messages(self,jid):
-        try: 
+    def messages(self,contact_id):
+        try:
+            jid = self.client.getRoster().getItem(contact_id)['jid']
             return self.messages_store[jid]
         except KeyError:
             return []
@@ -139,18 +248,21 @@ class XMPPSession():
 
     def add_contact(self,jid):
         self.client.getRoster().Subscribe(jid)
-        self.client.Process(0.1)
+        self.client.Process(0.5)
 
-    def remove_contact(self,jid):
+    def remove_contact(self,contact_id):
+        jid = self.client.getRoster().getItem(contact_id)['jid']
         self.client.getRoster().Unauthorize(jid)
         self.client.getRoster().Unsubscribe(jid)
         self.client.getRoster().delItem(jid)
 
-    def authorize_contact(self,jid):
+    def authorize_contact(self,contact_id):
+        jid = self.client.getRoster().getItem(contact_id)['jid']
         self.client.getRoster().Authorize(jid)
-        self.client.Process(0.1)
+        self.client.Process(0.5)
 
-    def unauthorize_contact(self,jid):
+    def unauthorize_contact(self,contact_id):
+        jid = self.client.getRoster().getItem(contact_id)['jid']
         self.client.getRoster().Unauthorize(jid)
 
 
@@ -172,11 +284,15 @@ class XMPPSessionThread(threading.Thread):
         
 
 class XMPPSessionPool():
-    def __init__(self):
+    def __init__(self,debug=False):
         self.session_pool = {}
+        self.debug = debug
         
     def start_session(self,jid,password,server=None):
-        session_id = jid
+        if  self.debug:
+            session_id = jid
+        else:
+            session_id = uuid.uuid4().hex
         self.session_pool[session_id] = XMPPSessionThread(XMPPSession(jid,password,server))
         self.session_pool[session_id].start()
         return session_id
@@ -201,7 +317,7 @@ class XMPPPlugin(object):
     api = 2
 
     def __init__(self,keyword = 'xmpp_pool'):
-        self.session_pool = XMPPSessionPool()
+        self.session_pool = XMPPSessionPool(debug=True)
         self.keyword = keyword
 
     def setup(self, app):
@@ -234,37 +350,3 @@ class XMPPPlugin(object):
 
     def __del__(self):
         self.session_pool.__del__()
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
