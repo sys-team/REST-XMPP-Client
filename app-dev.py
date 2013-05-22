7d78e0bdbb29b2530f3392b35e9192e13937c073
kovtash <kovtash@kovtash-mbp.unact.ru>
2013-05-22 18:21:06 +0400

    App termination handling

--- app-dev.py
+++ app-dev.py
@@ -3,6 +3,7 @@ __author__ = 'v.kovtash@gmail.com'
 
 import logging
 from app_builder import make_app
+import signal
 
 port = 6000
 host = '0.0.0.0'
@@ -11,5 +12,13 @@ dev_mode = True
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     app = make_app(dev_mode=dev_mode,push_notification_sender='apnwsgi')
+
+    def term_handler(signum = None, frame = None):
+        logging.info('Application termination started')
+        app.close()
+        logging.info('Application terminated')
+
+    signal.signal(signal.SIGTERM, term_handler)
+
     app.run(host=host, port=port, server='cherrypy') #reloader=True
-    app.close()
+
