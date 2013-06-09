58a5aa4e3adda220f3d5fab28cf028952910edef
kovtash <kovtash@kovtash-mbp.local>
2013-06-09 16:20:28 +0400

    event_id - itertools based id generator

--- xmpp_session_pool/event_id.py
+++ xmpp_session_pool/event_id.py
@@ -1,9 +1,7 @@
 __author__ = 'kovtash'
 
+import itertools
+
 class XMPPSessionEventID(object):
     def __init__(self):
-        self.__current_id = 0
-
-    def id(self):
-        self.__current_id +=1
-        return self.__current_id
+        self.id = itertools.count().next
