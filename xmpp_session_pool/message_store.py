3a924ea71979e586b4ff6c575da50279737af4bc
kovtash <kovtash@kovtash-mbp.unact.ru>
2013-08-07 18:38:26 +0400

    xmpp_session_pool.message_store - process delivery events

--- xmpp_session_pool/message_store.py
+++ xmpp_session_pool/message_store.py
@@ -25,11 +25,29 @@ class XMPPMessagesStore(PlugIn):
         contact_id = self._owner.getRoster().itemId(jid_from)
         contact = self._owner.getRoster().getItem(contact_id)
         message_text = event.getBody()
+        message_id = None
+        delivery_receipt_asked = False
+        requests = event.getTags('request')
+        received = event.getTag('received')
+
+        for request in requests:
+            if request.getAttr('xmlns') == 'urn:xmpp:receipts':
+                message_id = event.getID()
+                if  message_id is not None:
+                    delivery_receipt_asked = True
+
+        if received is not None:
+            message_id = received.getAttr('id')
+            if message_id is not None and contact_id in self.chats_store:
+                for message in self.chats_store[contact_id]:
+                    if not message['inbound'] and message['message_id'] == message_id:
+                        message['delivered'] = True
+            return
 
         if  message_text is not None and contact is not None:
-            self.append_message(contact_id=contact_id, inbound=True, text=message_text)
+            self.append_message(contact_id=contact_id, inbound=True, text=message_text, message_id=message_id, delivery_receipt_asked = delivery_receipt_asked)
 
-    def append_message(self, contact_id, inbound,text):
+    def append_message(self, contact_id, inbound, text, message_id = None, delivery_receipt_asked=False):
         if contact_id not in self.chats_store:
             self.chats_store[contact_id] = []
 
@@ -42,7 +60,10 @@ class XMPPMessagesStore(PlugIn):
                              'text':text[i:i+self.max_message_size],
                              'timestamp':timestamp,
                              'contact_id':contact_id,
-                             'chunk_id': i
+                             'chunk_id': i,
+                             'message_id': message_id,
+                             'delivered':False,
+                             'delivery_receipt_asked':delivery_receipt_asked
             })
 
         for message in messages:
@@ -66,6 +87,11 @@ class XMPPMessagesStore(PlugIn):
 
         if event_offset is not None:
             result = filter(lambda message: message['event_id'] > event_offset, result)
+
+        for message in result:
+            if message['inbound'] and message['delivery_receipt_asked'] and not message['delivered']:
+                self._owner.send_message_delivery_receipt(message['contact_id'],message['message_id'])
+                message['delivered']=True
 
         return result
 
